a9550cf41619a58134dfaeebb2b78811a0dd69c0
Carlos Araoz <caraoz@users.noreply.github.com>
2015-05-02 20:19:06 -0400

    add pre existing source file for collecting tweets

---
+++ collector.py
@@ -0,0 +1,156 @@
+import io
+import json
+import twitter
+import os
+import re
+
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
+ACCESS_TOKEN_KEY = ""
+ACCESS_TOKEN_SECRET = ""
+
+print("Enter Search Terms (may be delimited with commas")
+QUERY = input()
+search_terms = QUERY.split(',')
+search_term_map = {}
+for search_term in search_terms:
+    stm = {
+        'input_fp': ".".join([search_term, 'json']),
+        'output_fp': ".".join([search_term + '_processed', 'json'])
+    }
+    search_term_map[search_term] = stm
+
+def ke(x):
+	try:
+		if 'text' in x:
+			return str(x['text'])
+		else:
+			return " "
+	except:
+		return " "
+
+
+
+
+
+auth = twitter.oauth.OAuth(ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
+twitter_stream = twitter.TwitterStream(auth=auth)
+
+print('Filtering the public timeline for ' + QUERY)
+stream = twitter_stream.statuses.filter(track=QUERY)
+n = 0
+
+dummy = 0
+tk = []
+filtered = {}
+for tweet in stream:
+	filtered = {}
+	data = json.loads(json.dumps(tweet, ensure_ascii=False))
+	n = n + 1
+	nn = str(n) + " "
+	dummy = dummy + 1
+	for k, value in search_term_map.items() :
+		if re.search(k,ke(data),re.IGNORECASE) is not None:
+			try:
+				fh = os.open(search_term_map[k]['output_fp'],os.O_CREAT|os.O_APPEND|os.O_BINARY|os.O_RDWR)#####open file
+				
+				#ID UNIQUE KEY IF ANY
+				try:
+					id = tweet['id_str']
+				except KeyError:
+					id = dummy
+				filtered['id_str'] = id
+				
+				#find handle
+				try:
+					sn = tweet['user']['screen_name']
+				except KeyError:
+					sn = ""
+				filtered['screen_name'] = sn
+
+				#find time created
+				try:
+					twtime = tweet['created_at']
+				except KeyError:
+					twtime = ""
+				filtered['created_at'] = twtime
+				
+				#find text
+				try:
+					txt = tweet['retweeted_status']['text']
+					RT = True
+				except KeyError:
+					txt = tweet['text']
+					RT = False
+				filtered['text'] = txt
+				filtered['RT'] = RT
+				
+				#find account creation date
+				try:
+					userDate = tweet['user']['created_at']
+				except KeyError:
+					userDate = ""
+				filtered['account_date'] = userDate
+
+				#find bio info
+				try:
+					bio = tweet['user']['description']
+				except KeyError:
+					bio = ""
+				filtered['bio'] = bio
+
+					#get followers
+				try:
+					foll = tweet['user']['followers_count']
+				except KeyError:
+					foll = 0
+				filtered['followers'] = foll
+				
+				#get following
+				try:
+					following = tweet['user']['friends_count']
+				except KeyError:
+					following = 0
+				filtered['following'] = following
+
+				#get language
+				try:
+					ln = tweet['lang']
+				except KeyError:
+					ln = 0
+				filtered['lang'] = ln
+				#get location
+				try:
+					if tweet['coordinates'] is not None:
+						loc = tweet['coordinates']['coordinates']
+					else:
+						loc = []
+				except KeyError:
+					pass
+				filtered['loc'] = loc
+				
+				os.write(fh,bytes(u'{0}\n'.format(json.dumps(filtered, ensure_ascii=False)),'utf-8',errors='ignore'))
+				os.fsync(fh)
+				print(nn + tweet['user']['screen_name']  + "	" +    str(tweet['text'][:130].encode('utf-8').decode('ascii', 'ignore')), flush=True)
+			except:
+				continue
+		else:
+			pass
+	else:
+		pass
+
+
+	
+
+
+
+os.sleep(5)
+
+
+
+
+
+
+
+
+
